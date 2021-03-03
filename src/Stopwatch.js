28a89b86f0d74e8ce5cc56d66921cad60deb7307
Bryan Emerson <bryan.l.emerson@gmail.com>
2021-03-03 17:11:09 -0500

    done

--- src/Stopwatch.js
+++ src/Stopwatch.js
@@ -1,15 +1,48 @@
 import React, { Component } from 'react';
 import "./Stopwatch.css"
 
+
+
+
+
 class Stopwatch extends Component {
+  constructor() {
+    super()
+    this.intervalActive = false;
+    this.timer = null;
+    this.state = {
+      counter: 0
+    }
+  }
+  handleStart = () => {
+    if (this.intervalActive) return;
+    this.timer = setInterval(()=> {
+      this.setState({counter: this.state.counter + 1})
+    }, 1000);
+    this.intervalActive = true;
+  }
+
+  handlePause = () => {
+    clearInterval(this.timer)
+    this.intervalActive = false;
+  }
+
+  handleReset = ()=>{
+    console.log(this.state.counter)
+    this.intervalActive = false;
+    this.setState({counter: 0});
+    clearInterval(this.timer)
+  }
+
   render() {
+    console.log(this.state)
     return (
-      <div className="stopwatch">
-        <h1>0</h1>
+      <div className="stopwatch">{this.props.stopwatch}
+        <h1>{this.state.counter}</h1>
         <div className="controls">
-          <button>Reset</button>
-          <button>Start</button>
-          <button>Pause</button>
+          <button onClick={this.handleReset}>Reset</button>
+          <button onClick={this.handleStart}>Start</button>
+          <button onClick={this.handlePause}>Pause</button>
         </div>
       </div>
     );
